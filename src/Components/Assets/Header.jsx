cf08dcec7bf7350312f8d8a77bc7095f210ea05d
Subhadeep Roy <subha9.5roy350@gmail.com>
2023-10-29 17:06:35 +0000

    1

---
+++ src/Components/Assets/Header.jsx
@@ -0,0 +1,97 @@
+import React, { useState } from 'react';
+import { Link, useMatch, useResolvedPath } from 'react-router-dom';
+
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+  return (
+    <li className={isActive ? 'active' : ''}>
+      <Link
+        to={to}
+        {...props}
+        className={`block py-2 pr-4 pl-3 rounded lg:p-0 ${
+          isActive ? 'text-blue-500' : 'text-white'
+        }`}
+        aria-current="page"
+      >
+        {children}
+      </Link>
+    </li>
+  );
+}
+
+const Header = () => {
+  const [mobileMenuVisible, setMobileMenuVisible] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuVisible(!mobileMenuVisible);
+  };
+
+  return (
+    <header>
+      <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+          <Link to="/" className="flex items-center">
+            <img
+              src="https://i.postimg.cc/Hkz53rnY/q-removebg-preview.png"
+              className="mr-3 h-6 sm:h-9"
+              alt="Flowbite Logo"
+            />
+            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+              Quoverse
+            </span>
+          </Link>
+          <div className="flex items-center lg:order-2">
+            <button
+              onClick={toggleMobileMenu}
+              type="button"
+              className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="mobile-menu-2"
+              aria-expanded={mobileMenuVisible}
+            >
+              <svg
+                className={`w-6 h-6 ${mobileMenuVisible ? 'hidden' : ''}`}
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+              <svg
+                className={`w-6 h-6 ${mobileMenuVisible ? '' : 'hidden'}`}
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </button>
+          </div>
+          <div
+            className={`${
+              mobileMenuVisible ? 'block' : 'hidden'
+            } justify-between items-center w-full lg:flex  lg:order-1 right-0 lg:w-1/4 text-center`}
+            id="mobile-menu-2"
+          >
+            <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0 gap-6">
+              <CustomLink to="/">Home</CustomLink>
+              <CustomLink to="/about">About</CustomLink>
+              <CustomLink to="/team">Team</CustomLink>
+              <CustomLink to="/contact">Contact</CustomLink>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
