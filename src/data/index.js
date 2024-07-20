7c5cbc209556e83671b9964b2792a91904f354ab
Subhadeep Roy <subha9.5roy350@gmail.com>
2024-07-20 09:27:12 +0530

    Update index.js

--- src/data/index.js
+++ src/data/index.js
@@ -1,4 +1,541 @@
-import tags from "./tags";
+const tags = [
+  {
+    "_id": "PIyOMHYqPd",
+    "name": "Age",
+    "slug": "age",
+    "quoteCount": 1,
+    "dateAdded": "2023-04-14",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "qTFouZDPBZz",
+    "name": "Athletics",
+    "slug": "athletics",
+    "quoteCount": 0,
+    "dateAdded": "2022-07-06",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "M83oc3scg",
+    "name": "Business",
+    "slug": "business",
+    "quoteCount": 31,
+    "dateAdded": "2019-08-03",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "QmvdN2qkQCC",
+    "name": "Change",
+    "slug": "change",
+    "quoteCount": 34,
+    "dateAdded": "2022-03-12",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "vWfmIQt6k8c",
+    "name": "Character",
+    "slug": "character",
+    "quoteCount": 17,
+    "dateAdded": "2022-03-12",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "JaQwywHSk59",
+    "name": "Competition",
+    "slug": "competition",
+    "quoteCount": 38,
+    "dateAdded": "2022-07-06",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "AjxQFSPEylb",
+    "name": "Conservative",
+    "slug": "conservative",
+    "quoteCount": 1,
+    "dateAdded": "2022-03-12",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "Ipw2-EcInZg",
+    "name": "Courage",
+    "slug": "courage",
+    "quoteCount": 2,
+    "dateAdded": "2022-03-12",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "aLWQOIYpeMz",
+    "name": "Creativity",
+    "slug": "creativity",
+    "quoteCount": 1,
+    "dateAdded": "2023-04-14",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "kqzFRe-4V4",
+    "name": "Education",
+    "slug": "education",
+    "quoteCount": 7,
+    "dateAdded": "2019-03-17",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "jl_H9UVXhGb",
+    "name": "Ethics",
+    "slug": "ethics",
+    "quoteCount": 1,
+    "dateAdded": "2023-04-06",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "gElqvJIRz0h",
+    "name": "Failure",
+    "slug": "failure",
+    "quoteCount": 1,
+    "dateAdded": "2023-04-14",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "-7GEUrC5r",
+    "name": "Faith",
+    "slug": "faith",
+    "quoteCount": 4,
+    "dateAdded": "2019-09-13",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "r437PEqVZx5",
+    "name": "Family",
+    "slug": "family",
+    "quoteCount": 2,
+    "dateAdded": "2022-03-12",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "fvpORe-t",
+    "name": "Famous Quotes",
+    "slug": "famous-quotes",
+    "quoteCount": 1090,
+    "dateAdded": "2019-07-23",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "HJ05xaA6gN",
+    "name": "Film",
+    "slug": "film",
+    "quoteCount": 13,
+    "dateAdded": "2022-07-04",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "QuBdKRcjNsO",
+    "name": "Freedom",
+    "slug": "freedom",
+    "quoteCount": 9,
+    "dateAdded": "2022-03-12",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "krXU-q4FE",
+    "name": "Friendship",
+    "slug": "friendship",
+    "quoteCount": 189,
+    "dateAdded": "2019-03-15",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "OMnUd1CUg",
+    "name": "Future",
+    "slug": "future",
+    "quoteCount": 21,
+    "dateAdded": "2019-02-17",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "i_OFrgCiQ4",
+    "name": "Generosity",
+    "slug": "generosity",
+    "quoteCount": 1,
+    "dateAdded": "2023-04-14",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "L09SJrfjY8s",
+    "name": "Genius",
+    "slug": "genius",
+    "quoteCount": 1,
+    "dateAdded": "2023-04-14",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "S9OYJZlyu5",
+    "name": "Gratitude",
+    "slug": "gratitude",
+    "quoteCount": 1,
+    "dateAdded": "2023-04-14",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "k97A51Uf5",
+    "name": "Happiness",
+    "slug": "happiness",
+    "quoteCount": 18,
+    "dateAdded": "2019-06-27",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "kv9zk8WIqaq",
+    "name": "Health",
+    "slug": "health",
+    "quoteCount": 1,
+    "dateAdded": "2023-04-14",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "KBWnDu4rH",
+    "name": "History",
+    "slug": "history",
+    "quoteCount": 18,
+    "dateAdded": "2021-01-30",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "uCLiEwnwh",
+    "name": "Honor",
+    "slug": "honor",
+    "quoteCount": 6,
+    "dateAdded": "2022-07-04",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "uki-krBKTB_",
+    "name": "Humor",
+    "slug": "humor",
+    "quoteCount": 0,
+    "dateAdded": "2022-03-12",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "HJp_e1usX",
+    "name": "Humorous",
+    "slug": "humorous",
+    "quoteCount": 17,
+    "dateAdded": "2022-07-08",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "eghR-r-OzL9",
+    "name": "Imagination",
+    "slug": "imagination",
+    "quoteCount": 2,
+    "dateAdded": "2023-04-06",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "JCMoLDds9",
+    "name": "Inspirational",
+    "slug": "inspirational",
+    "quoteCount": 89,
+    "dateAdded": "2020-01-27",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "v9QUFHDZPT3",
+    "name": "Knowledge",
+    "slug": "knowledge",
+    "quoteCount": 3,
+    "dateAdded": "2023-04-06",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "V60a195td",
+    "name": "Leadership",
+    "slug": "leadership",
+    "quoteCount": 1,
+    "dateAdded": "2022-07-04",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "poT-7QEBm",
+    "name": "Life",
+    "slug": "life",
+    "quoteCount": 42,
+    "dateAdded": "2020-09-09",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "3n-gucf_OB",
+    "name": "Literature",
+    "slug": "literature",
+    "quoteCount": 1,
+    "dateAdded": "2020-03-01",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "rnrd8q9X1",
+    "name": "Love",
+    "slug": "love",
+    "quoteCount": 20,
+    "dateAdded": "2020-02-27",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "s19tg5r8EM-",
+    "name": "Mathematics",
+    "slug": "mathematics",
+    "quoteCount": 1,
+    "dateAdded": "2023-04-06",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "CaE-vzeOZb",
+    "name": "Motivational",
+    "slug": "motivational",
+    "quoteCount": 32,
+    "dateAdded": "2022-07-04",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "vmVZQ72P_",
+    "name": "Nature",
+    "slug": "nature",
+    "quoteCount": 2,
+    "dateAdded": "2020-12-10",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "pnLPcXTs_S",
+    "name": "Opportunity",
+    "slug": "opportunity",
+    "quoteCount": 1,
+    "dateAdded": "2023-04-14",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "q8kOLaefsZM",
+    "name": "Pain",
+    "slug": "pain",
+    "quoteCount": 1,
+    "dateAdded": "2022-03-12",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "GaQEsvfbYYd",
+    "name": "Perseverance",
+    "slug": "perseverance",
+    "quoteCount": 2,
+    "dateAdded": "2023-04-14",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "mh6HEhK_T_a",
+    "name": "Philosophy",
+    "slug": "philosophy",
+    "quoteCount": 16,
+    "dateAdded": "2022-03-12",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "96NNdxeI_",
+    "name": "Politics",
+    "slug": "politics",
+    "quoteCount": 15,
+    "dateAdded": "2020-04-02",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "MbsuUl67N3",
+    "name": "Power Quotes",
+    "slug": "power-quotes",
+    "quoteCount": 2,
+    "dateAdded": "2022-07-04",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "p5xRCWCdU",
+    "name": "Proverb",
+    "slug": "proverb",
+    "quoteCount": 0,
+    "dateAdded": "2021-02-12",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "tWLCsyf_K",
+    "name": "Religion",
+    "slug": "religion",
+    "quoteCount": 6,
+    "dateAdded": "2020-01-12",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "dm15bka7Qc",
+    "name": "Sadness",
+    "slug": "sadness",
+    "quoteCount": 1,
+    "dateAdded": "2023-04-14",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "qO4zwIUdFW",
+    "name": "Science",
+    "slug": "science",
+    "quoteCount": 14,
+    "dateAdded": "2020-01-26",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "5wvH2mbETdq",
+    "name": "Self",
+    "slug": "self",
+    "quoteCount": 6,
+    "dateAdded": "2022-03-12",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "IJsMcEupo4",
+    "name": "Self Help",
+    "slug": "self-help",
+    "quoteCount": 6,
+    "dateAdded": "2022-07-04",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "-WCNo8uFORU",
+    "name": "Social Justice",
+    "slug": "social-justice",
+    "quoteCount": 3,
+    "dateAdded": "2022-03-12",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "eD6qAIcDR8s",
+    "name": "Society",
+    "slug": "society",
+    "quoteCount": 1,
+    "dateAdded": "2023-04-06",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "DNZ4IdtHiFG",
+    "name": "Spirituality",
+    "slug": "spirituality",
+    "quoteCount": 4,
+    "dateAdded": "2022-03-12",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "EKV8W1TN-wb",
+    "name": "Sports",
+    "slug": "sports",
+    "quoteCount": 43,
+    "dateAdded": "2022-07-06",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "kZei477Cojv",
+    "name": "Stupidity",
+    "slug": "stupidity",
+    "quoteCount": 1,
+    "dateAdded": "2023-04-06",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "wm1HFcO8vf",
+    "name": "Success",
+    "slug": "success",
+    "quoteCount": 82,
+    "dateAdded": "2020-10-14",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "Gq75KBrfb",
+    "name": "Technology",
+    "slug": "technology",
+    "quoteCount": 50,
+    "dateAdded": "2020-01-27",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "LzQ9iXOoZw_",
+    "name": "Time",
+    "slug": "time",
+    "quoteCount": 1,
+    "dateAdded": "2022-03-12",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "o7BP9_4e2lL",
+    "name": "Tolerance",
+    "slug": "tolerance",
+    "quoteCount": 1,
+    "dateAdded": "2023-04-06",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "B1O_IThWjSP",
+    "name": "Truth",
+    "slug": "truth",
+    "quoteCount": 7,
+    "dateAdded": "2022-03-12",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "bsT8Bb9sxB",
+    "name": "Virtue",
+    "slug": "virtue",
+    "quoteCount": 10,
+    "dateAdded": "2022-07-04",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "olEL606Ju49",
+    "name": "War",
+    "slug": "war",
+    "quoteCount": 3,
+    "dateAdded": "2022-03-12",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "AN2qILFNzW",
+    "name": "Weakness",
+    "slug": "weakness",
+    "quoteCount": 1,
+    "dateAdded": "2023-04-14",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "5j5s-YkHAr_",
+    "name": "Wellness",
+    "slug": "wellness",
+    "quoteCount": 1,
+    "dateAdded": "2023-04-14",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "6J1qxxuj3",
+    "name": "Wisdom",
+    "slug": "wisdom",
+    "quoteCount": 550,
+    "dateAdded": "2019-10-18",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "Y3mg6WH7Qv1",
+    "name": "Work",
+    "slug": "work",
+    "quoteCount": 3,
+    "dateAdded": "2023-04-14",
+    "dateModified": "2023-04-14"
+  },
+  {
+    "_id": "NLC25zc7-m5",
+    "name": "Work",
+    "slug": "work",
+    "quoteCount": 3,
+    "dateAdded": "2023-04-14",
+    "dateModified": "2023-04-14"
+  }
+]
 
 const getTags = async () => {
         const slugs = tags.map(tag => tag.slug);
