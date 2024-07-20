de839495a8a644a9bb05de75f24a4178b141cc11
Subhadeep Roy <subha9.5roy350@gmail.com>
2024-07-20 09:23:08 +0530

    Update index.js

--- src/data/index.js
+++ src/data/index.js
@@ -1,20 +1,8 @@
-const api = process.env.REACT_APP_API_KEY;
-
-const url = `https://${api}/tags`;
+import tags from "./tags";
 
 const getTags = async () => {
-    try {
-        const response = await fetch(url); 
-        if (!response.ok) {
-            throw new Error("Failed to fetch data");
-        }
-        const data = await response.json();
-        const slugs = data.map(tag => tag.slug);
+        const slugs = tags.map(tag => tag.slug);
         return slugs;
-    } catch (error) {
-        console.error(error);
-        return [];
-    }
 };
 
 export { getTags };
